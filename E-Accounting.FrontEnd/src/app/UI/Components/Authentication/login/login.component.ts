240e02f334b2c28ec8ec47afc30df62ab584ba9a
EmirSnmezz <emircan_snmez@outlook.com>
2024-09-13 14:39:01 +0300

    Login Page Fixed, Template Driven Form Added

--- E-Accounting.FrontEnd/src/app/UI/Components/Authentication/login/login.component.ts
+++ E-Accounting.FrontEnd/src/app/UI/Components/Authentication/login/login.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
+import {FormsModule} from '@angular/forms'
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterLink, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  formRequest(){
+
+  }
 
 }
