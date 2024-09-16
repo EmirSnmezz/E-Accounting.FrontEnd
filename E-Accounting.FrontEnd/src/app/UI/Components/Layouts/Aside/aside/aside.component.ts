4d3a601ffc9b526b3f9befc4f28974389a56ff6d
EmirSnmezz <emircan_snmez@outlook.com>
2024-09-16 17:47:27 +0300

    template was torn apart and aside component created (step 1/3)

---
+++ E-Accounting.FrontEnd/src/app/UI/Components/Layouts/Aside/aside/aside.component.ts
@@ -0,0 +1,27 @@
+import { Component, Inject, InjectionToken, Input, PLATFORM_ID } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+
+import { AuthenticationService } from '../../../Authentication/Services/authentication.service';
+import { LoginResponseModel } from '../../../Authentication/Models/login-response.model';
+import { Navigations } from '../../../../router/navigation';
+@Component({
+  selector: 'app-aside',
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: './aside.component.html',
+  styleUrl: './aside.component.css'
+})
+export class AsideComponent {
+  @Input()loginResponseModel = new LoginResponseModel();
+  navigations = Navigations
+
+  constructor(private _router: Router, private _authencticationService: AuthenticationService)
+  {}
+
+  logout()
+  {
+    localStorage.clear();
+    this._authencticationService.logout();
+  }
+}
