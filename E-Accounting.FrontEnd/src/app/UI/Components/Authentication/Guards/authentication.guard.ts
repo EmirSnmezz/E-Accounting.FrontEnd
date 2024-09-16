b0f1aadf22f5d2635fbce45a7ec65e507b5ad9ca
EmirSnmezz <emircan_snmez@outlook.com>
2024-09-16 10:41:25 +0300

    .

--- E-Accounting.FrontEnd/src/app/UI/Components/Authentication/Guards/authentication.guard.ts
+++ E-Accounting.FrontEnd/src/app/UI/Components/Authentication/Guards/authentication.guard.ts
@@ -7,31 +7,32 @@ import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, CanActivateChild
 })
 
 export class AuthenticationGuard implements CanActivate, CanActivateChild {
-  isBrowser : boolean ;
+  isBrowser: boolean;
   token;
   state: boolean = false;
-  constructor(private _router: Router, @Inject(PLATFORM_ID) private _platformId) 
-  {
-    if(isPlatformBrowser(_platformId))
-    {
+  constructor(private _router: Router, @Inject(PLATFORM_ID) private _platformId) {
+    if (isPlatformBrowser(_platformId)) {
       this.isBrowser = true;
       this.token = localStorage.getItem("accessToken");
     }
-    else{
+    else {
       this.isBrowser = false;
     }
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    if(this.isBrowser)
-    {
+    if (this.isBrowser) {
       if (this.token == null) {
         this._router.navigateByUrl("/login");
         return this.state;
       }
+      else {
+        this.state = true;
+        return this.state;
+      }
     }
-  this.state = true;
-  return this.state;
+
+    return this.state;
 
   }
 
