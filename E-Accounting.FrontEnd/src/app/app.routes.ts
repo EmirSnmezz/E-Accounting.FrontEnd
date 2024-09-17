52c7bca6eba479fab3f5a187d0a7502949c43967
EmirSnmezz <emircan_snmez@outlook.com>
2024-09-17 16:14:22 +0300

    add ucaf route in routes

--- E-Accounting.FrontEnd/src/app/app.routes.ts
+++ E-Accounting.FrontEnd/src/app/app.routes.ts
@@ -1,24 +1,30 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { AuthenticationGuard } from './UI/Components/Authentication/Guards/authentication.guard';
 
-export const routes: Routes = 
-[
-    {
-        path : "",
-        canActivateChild : [AuthenticationGuard],
-        loadComponent: ()=> import("./UI/Components/Layouts/layout/layout.component")
-        .then(c => c.LayoutComponent),
-        children : [
-            {
-                path : "", 
-                loadComponent : ()=> import("./UI/Components/Blank/blank.component")
-                .then( c => c.BlankComponent)}
-        ]
-        
-    },
-    {
-        path:"login",
-        loadComponent: () => import("./UI/Components/Authentication/login/login.component").then(x => x.LoginComponent)
-    }
-];
+export const routes: Routes =
+    [
+        {
+            path: "",
+            canActivateChild: [AuthenticationGuard],
+            loadComponent: () => import("./UI/Components/Layouts/layout/layout.component")
+                .then(c => c.LayoutComponent),
+            children: [
+                {
+                    path: "",
+                    loadComponent: () => import("./UI/Components/Blank/blank.component")
+                        .then(c => c.BlankComponent)
+                },
+                {
+                    path: "ucafs",
+                    loadComponent: () => import("./UI/Components/UCAFs/ucafs/ucafs.component")
+                        .then(c => c.UcafsComponent)
+                }
+            ]
+
+        },
+        {
+            path: "login",
+            loadComponent: () => import("./UI/Components/Authentication/Login/login.component")
+                .then(x => x.LoginComponent)
+        }
+    ];
