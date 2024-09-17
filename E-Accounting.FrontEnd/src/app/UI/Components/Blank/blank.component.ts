e33abcc32d25c2a290f34cab58e3a934f17aef33
EmirSnmezz <emircan_snmez@outlook.com>
2024-09-17 16:12:43 +0300

    blank component initilazed blank-common-component

--- E-Accounting.FrontEnd/src/app/UI/Components/Blank/blank.component.ts
+++ E-Accounting.FrontEnd/src/app/UI/Components/Blank/blank.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { SectionComponent } from "../../../Common/Components/Blank/blank/Section/section/section.component";
+import { BlankCommonComponent } from '../../../Common/Components/Blank/blank/blank.component';
+import { NavModel } from '../../../Common/Components/Blank/blank/Models/Nav.model';
 
 @Component({
   selector: 'app-blank',
   standalone: true,
-  imports: [],
+  imports: [BlankComponent, SectionComponent, BlankCommonComponent],
   templateUrl: './blank.component.html',
   styleUrl: './blank.component.css'
 })
 export class BlankComponent {
-
+title: string = "Anasayfa"
+sectionTitle: string = "Anasayfa"
+navs: NavModel[] = [
+  {
+    routerLink : "",
+    class: "test",
+    name : "Anasayfa"
+  }
+]
 }
