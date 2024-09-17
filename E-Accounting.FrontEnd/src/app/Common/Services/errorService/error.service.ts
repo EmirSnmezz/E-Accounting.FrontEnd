5ef803c20f035775e6b63a22f59b30ad022a729d
EmirSnmezz <emircan_snmez@outlook.com>
2024-09-17 03:01:24 +0300

    error service applied

--- E-Accounting.FrontEnd/src/app/Common/Services/errorService/error.service.ts
+++ E-Accounting.FrontEnd/src/app/Common/Services/errorService/error.service.ts
@@ -1,21 +1,37 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { log } from 'console';
+import { ToastrService, ToastrTypes } from '../ToastrService/toastr.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorService {
 
-  constructor() { }
+  constructor(private _toastr: ToastrService) { }
 
   errorHandler(err: HttpErrorResponse)
-  {
-    if(err.status == 0){
-      //işlemler
-    }
+  { debugger;
+    switch(err.status){
+      case 0:
+        this._toastr.toast(ToastrTypes.Error, "Hata!", "Api adresine ulaşılamıyor!");
+        break;
 
+        case 404:
+          this._toastr.toast(ToastrTypes.Error, "Hata!", "Api adresi bulunamıyor!");
+          break;
+          case 500:
+            if(err.error){
+              let message = JSON.stringify(err.error.Message)
+              this._toastr.toast(ToastrTypes.Error, "Hata!", message);
+            }else{
+              this._toastr.toast(ToastrTypes.Error, "Hata!", "Bir hata oluştu!");
+            }
+
+              break;
+      default:
+        break;
+    }
     console.log(err.message);
-    
   }
 }
--- E-Accounting.FrontEnd/src/app/UI/Components/Authentication/login/login.component.ts
+++ E-Accounting.FrontEnd/src/app/UI/Components/Authentication/login/login.component.ts
@@ -5,7 +5,6 @@ import { FormsModule, NgForm} from '@angular/forms'
 import { InputValidDirective } from '../../../../Common/Directives/Input-validtation-directive/input-valid.directive';
 import { LoadingButtonComponent } from "../../../../Common/Components/loading-button/loading-button.component";
 import { AuthenticationService } from '../Services/authentication.service';
-import { ToastrService, ToastrTypes } from '../../../../Common/Services/ToastrService/toastr.service';
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -16,9 +15,8 @@ import { ToastrService, ToastrTypes } from '../../../../Common/Services/ToastrSe
 export class LoginComponent {
 
   isLoading: boolean = false;
-  constructor(private _authenticationService: AuthenticationService, private _toastr: ToastrService)
+  constructor(private _authenticationService: AuthenticationService)
   {
-    _toastr.toast(ToastrTypes.Success, "Deneme Başlık", "Deneme İçerik");
   }
   Login(form : NgForm) {
 
