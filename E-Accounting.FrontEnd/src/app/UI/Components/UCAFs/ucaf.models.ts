c2a742249a20a4ba935551657510c3524e5d752f
EmirSnmezz <emircan_snmez@outlook.com>
2024-09-20 02:12:24 +0300

    ucaf model created for It is the equivalent of the data coming from GetAllUCAF apiendpoint

---
+++ E-Accounting.FrontEnd/src/app/UI/Components/UCAFs/ucaf.models.ts
@@ -0,0 +1,8 @@
+export class UCAFModel{
+    id: string = "";
+    code: string = "";
+    type: string = "";
+    name: string = "";
+    createdDate: string = "";
+    updatedDate: string = "";
+}
