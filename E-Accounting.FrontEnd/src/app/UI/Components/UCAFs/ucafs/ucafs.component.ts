ec188e43d7db88ed3adb16ae347cbf339575c54e
EmirSnmezz <emircan_snmez@outlook.com>
2024-09-20 02:14:10 +0300

    ucaf component html was fixed for view data from coming api endpoint

--- E-Accounting.FrontEnd/src/app/UI/Components/UCAFs/ucafs/ucafs.component.ts
+++ E-Accounting.FrontEnd/src/app/UI/Components/UCAFs/ucafs/ucafs.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 import { BlankCommonComponent } from '../../../../Common/Components/Blank/blank/blank.component';
 import { SectionComponent } from '../../../../Common/Components/Blank/blank/Section/section/section.component';
 import { NavModel } from '../../../../Common/Components/Blank/blank/Models/Nav.model';
+import { UCAFService } from '../Services/ucaf.service';
+import { UCAFModel } from '../ucaf.models';
+import { ResponseModel } from '../../../../Common/Models/response.model';
 
 @Component({
   selector: 'app-ucafs',
@@ -17,6 +20,18 @@ navs: NavModel[] = [
     routerLink : "/",
     class : "",
     name : "Hesap Planı"
+  }]
+
+  constructor(private _ucafService: UCAFService){}
+
+  ucafs: UCAFModel[] = [];
+
+  ngOnInit(){
+    this.getAll();
   }
-]
+
+  getAll()
+  {
+    this._ucafService.getAll(res => this.ucafs = res.data)
+  }
 }
