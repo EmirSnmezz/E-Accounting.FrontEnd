ab45a196760d20986691a34bdfa620b0d75af7e9
EmirSnmezz <emircan_snmez@outlook.com>
2024-09-20 16:03:28 +0300

    added companyId property in ucafModel. Because companyId required for CRUD operations

--- E-Accounting.FrontEnd/src/app/UI/Components/UCAFs/ucaf.models.ts
+++ E-Accounting.FrontEnd/src/app/UI/Components/UCAFs/ucaf.models.ts
@@ -1,8 +1,9 @@
 export class UCAFModel{
     id: string = "";
     code: string = "";
-    type: string = "";
+    type: string = '';
     name: string = "";
     createdDate: string = "";
     updatedDate: string = "";
+    companyId: string = "";
 }
