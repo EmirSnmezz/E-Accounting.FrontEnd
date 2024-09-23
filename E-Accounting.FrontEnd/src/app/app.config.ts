c90c5bf58fd7f0c54b02594627f7e4e78cb81792
EmirSnmezz <emircan_snmez@outlook.com>
2024-09-23 18:39:47 +0300

    sweetAlert library provided

--- E-Accounting.FrontEnd/src/app/app.config.ts
+++ E-Accounting.FrontEnd/src/app/app.config.ts
@@ -1,10 +1,10 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-
+import {SweetAlert2Module} from "@sweetalert2/ngx-sweetalert2"
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()), importProvidersFrom(BrowserModule, SweetAlert2Module.forRoot())]
 };
