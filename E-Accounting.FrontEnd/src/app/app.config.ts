3bd84a78330d2107645d3f35014ab72c87f660eb
EmirSnmezz <emircan_snmez@outlook.com>
2024-09-25 14:17:05 +0300

    storeModude provided on prroject in app.confing.ts

--- E-Accounting.FrontEnd/src/app/app.config.ts
+++ E-Accounting.FrontEnd/src/app/app.config.ts
@@ -4,7 +4,10 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import {SweetAlert2Module} from "@sweetalert2/ngx-sweetalert2"
+import {SweetAlert2Module} from "@sweetalert2/ngx-sweetalert2";
+import { provideStore, StoreModule } from '@ngrx/store'
+import { changeLoading } from './Common/State/loading.actions';
+import { loadingReducer } from './Common/State/loading-reducer';
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()), importProvidersFrom(BrowserModule, SweetAlert2Module.forRoot())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()), importProvidersFrom(BrowserModule, SweetAlert2Module.forRoot(), StoreModule.forRoot({loading: loadingReducer})), provideStore()]
 };
