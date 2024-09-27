2195f4d1d4004b2335030ad544d3a2cc968c6933
EmirSnmezz <emircan_snmez@outlook.com>
2024-09-27 11:26:46 +0300

    getToken process out of constructor

--- E-Accounting.FrontEnd/src/app/Common/Services/loginResponseService/login-response.service.ts
+++ E-Accounting.FrontEnd/src/app/Common/Services/loginResponseService/login-response.service.ts
@@ -1,9 +1,6 @@
 import { Inject, Injectable, PLATFORM_ID } from "@angular/core";
 import { LoginResponseModel } from "../../../UI/Components/Authentication/Models/login-response.model";
 import { CryptoService } from "../cryptoService/crypto.service";
-import { GenericHttpClientService } from "../HttpService/generic-http-clients.service";
-import { platform } from "os";
-
 declare var localStorage;
 
 @Injectable({
@@ -14,16 +11,14 @@ export class LoginResponseService {
 
     loginResponseModel: LoginResponseModel = new LoginResponseModel();
 
-    constructor(private _cyrptoService: CryptoService, @Inject(PLATFORM_ID) _platform) {
-        let token = localStorage.getItem("accessToken")?.toString();
-        if (token != undefined) {
-            let loginResponseString = _cyrptoService.decrypto(token);
-            this.loginResponseModel = JSON.parse(loginResponseString)
-        }
-    }
+    constructor(private _cyrptoService: CryptoService, @Inject(PLATFORM_ID) _platform) {}
 
     getLoginReponseModel() {
+        var token = localStorage.getItem("accessToken")?.toString();
+        if(token != undefined){
+        let loginResponseString = this._cyrptoService.decrypto(token);
+        this.loginResponseModel = JSON.parse(loginResponseString)
+        }        
         return this.loginResponseModel;
     }
-
 }
