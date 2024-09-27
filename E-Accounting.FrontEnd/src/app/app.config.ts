b2e5eddcfff6801d3d6c65eee5d4893fce6e83b1
EmirSnmezz <emircan_snmez@outlook.com>
2024-09-27 11:22:57 +0300

    State managment provided in app.config

--- E-Accounting.FrontEnd/src/app/app.config.ts
+++ E-Accounting.FrontEnd/src/app/app.config.ts
@@ -6,8 +6,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import {SweetAlert2Module} from "@sweetalert2/ngx-sweetalert2";
 import { provideStore, StoreModule } from '@ngrx/store'
-import { changeLoading } from './Common/State/loading.actions';
-import { loadingReducer } from './Common/State/loading-reducer';
+import { changeLoading } from './Common/State/loading/loading.actions';
+import { loadingReducer } from './Common/State/loading/loading-reducer';
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()), importProvidersFrom(BrowserModule, SweetAlert2Module.forRoot(), StoreModule.forRoot({loading: loadingReducer})), provideStore()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideClientHydration(), 
+    provideHttpClient(withFetch()), 
+    importProvidersFrom(BrowserModule, 
+      SweetAlert2Module.forRoot(), 
+      StoreModule.forRoot({loading: loadingReducer}))
+    ]
 };
