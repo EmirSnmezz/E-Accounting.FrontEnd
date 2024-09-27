74715a62040c81f2486e5f4c94613eecb8097f5d
EmirSnmezz <emircan_snmez@outlook.com>
2024-09-27 11:23:40 +0300

    state managment applied loading button

--- E-Accounting.FrontEnd/src/app/Common/Components/loading-button/loading-button.component.ts
+++ E-Accounting.FrontEnd/src/app/Common/Components/loading-button/loading-button.component.ts
@@ -1,20 +1,29 @@
-import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { NgForm } from '@angular/forms';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'loading-button',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './loading-button.component.html',
-  styleUrl: './loading-button.component.css'
+  styleUrls: ['./loading-button.component.css']
 })
-export class LoadingButtonComponent {
-  // isLoading: boolean = false;
-  @Input("form") form: NgForm;
-  @Input("isLoading") isLoading: boolean = false;
-  @Input("btnName") btnName: string = "";
-  @Input("btnLoadingDescription") btnLoadingDescription: string = "";
-  @Input("btnClass") btnClass: string = "btn btn-primary btn-block w-100";
-  @Input("BtnIconClass") btnIconClass: string = "";
+export class LoadingButtonComponent { 
+  isloading: boolean = false; //state
+  @Input() form: NgForm;
+  @Input() btnName: string = "";
+  @Input() btnLoadingDescription: string = "";
+  @Input() btnClass: string = "btn-outline-primary w-100";
+  @Input() btnLoadingClass: string = "btn-outline-primary w-100";
+  @Input() iconClass: string  = "fa fa-save";
+
+  constructor(
+    private store: Store<{loading: boolean}>
+  ){
+    this.store.select("loading").subscribe(res=>{
+      this.isloading = res;
+    });
+  }
 }
