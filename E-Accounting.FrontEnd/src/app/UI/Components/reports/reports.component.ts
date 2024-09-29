6ce75c1b89106dc6f1cc605651e74e6ae18f3bc0
EmirSnmezz <emircan_snmez@outlook.com>
2024-09-29 17:45:34 +0300

    report component's ts file created. NavModel and Report Model Added.

--- E-Accounting.FrontEnd/src/app/UI/Components/reports/reports.component.ts
+++ E-Accounting.FrontEnd/src/app/UI/Components/reports/reports.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { BlankComponent } from "../Blank/blank.component";
+import { SectionComponent } from "../../../Common/Components/Blank/blank/Section/section/section.component";
+import { NavModel } from '../../../Common/Components/Blank/blank/Models/Nav.model';
+import { BlankCommonComponent } from '../../../Common/Components/Blank/blank/blank.component';
+import { ReportModel } from './models/report.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-reports',
   standalone: true,
-  imports: [],
+  imports: [BlankCommonComponent, SectionComponent, DatePipe],
   templateUrl: './reports.component.html',
   styleUrl: './reports.component.css'
 })
 export class ReportsComponent {
+  navs: NavModel[] = [
+    {
+      routerLink: "/",
+      class: "",
+      name: "Ana Sayfa"
+    },
+    {
+      routerLink: "/reports",
+      class: "active",
+      name: "Raporlar"
+    }
+  ]
 
+  reports: ReportModel[] = []
 }
