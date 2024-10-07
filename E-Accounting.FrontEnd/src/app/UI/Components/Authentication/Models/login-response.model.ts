bde0615f77bfe6956dbac2eb733c70cf52944e28
EmirSnmezz <emircan_snmez@outlook.com>
2024-10-07 21:21:29 +0300

    setted for default settings of login response model

--- E-Accounting.FrontEnd/src/app/UI/Components/Authentication/Models/login-response.model.ts
+++ E-Accounting.FrontEnd/src/app/UI/Components/Authentication/Models/login-response.model.ts
@@ -4,9 +4,9 @@ import { TokenModel } from "./token.model";
 export class LoginResponseModel {
     token: TokenModel = new TokenModel();
     email: string = "";
-    userId: string
+    userId: string = "";
     userNameAndLastName: string = "";
     companies: CompanyModel[] = [];
     company: CompanyModel = new CompanyModel();
-    year: number = 0;
+    year: number = new Date().getFullYear();
 }
