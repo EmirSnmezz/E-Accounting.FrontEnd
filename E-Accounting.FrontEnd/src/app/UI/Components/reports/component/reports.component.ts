539d033747c988fad79cd9a6ddf7665ef134ef58
Emir Sönmez <79750775+EmirSnmezz@users.noreply.github.com>
2024-10-28 19:34:04 +0300

    report components updated

--- E-Accounting.FrontEnd/src/app/UI/Components/reports/component/reports.component.ts
+++ E-Accounting.FrontEnd/src/app/UI/Components/reports/component/reports.component.ts
@@ -4,21 +4,22 @@ import { SectionComponent } from "../../../../Common/Components/Blank/blank/Sect
 import { NavModel } from '../../../../Common/Components/Blank/blank/Models/Nav.model';
 import { BlankCommonComponent } from '../../../../Common/Components/Blank/blank/blank.component';
 import { ReportModel } from '../models/report.model';
-import { DatePipe } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { ReportService } from '../services/report.service';
+import { get } from 'jquery';
 
 @Component({
   selector: 'app-reports',
   standalone: true,
-  imports: [BlankCommonComponent, SectionComponent, DatePipe],
+  imports: [BlankCommonComponent, SectionComponent, DatePipe, CommonModule],
   templateUrl: './reports.component.html',
   styleUrl: './reports.component.css'
 })
 export class ReportsComponent {
 
   reports: ReportModel[] = [];
-
   navs: NavModel[] = []
+  reportStatusText: string = "";
 
   constructor(private _report: ReportService)
   {
@@ -33,7 +34,7 @@ export class ReportsComponent {
 
   ngOnInit()
   {
-    throw new Error('Method Not İmplemented');
+    this.getAll();
   }
 
   getAll()
@@ -43,9 +44,13 @@ export class ReportsComponent {
 
   changeSpanClassByStatus(status: boolean)
   {
-    if(status)
+    if(status){
+      this.reportStatusText = "Hazır...";
       return "text-success";
-
-    return "text-danger";
+    }
+    else{
+      this.reportStatusText = "Hazırlanıyor...."
+      return "text-danger";
+    }
   }
 }
